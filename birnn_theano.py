8eb9cee557edcd78ac018f30dcdfeafa6b23990e
Anurag Roy <anu15roy@gmail.com>
2016-06-25 14:55:42 +0530

    removed connection beween hf and hb

--- birnn_theano.py
+++ birnn_theano.py
@@ -21,14 +21,14 @@ class BIRNN(object):
 	    self.Whh_f = theano.shared(name='Whh_f', value=Whh_f.astype(theano.config.floatX))
 
 	    Wxh_b = np.random.uniform(-np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, word_dim))
-	    Whh_fb = np.random.uniform(-np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, hidden_dim))
+	    # Whh_fb = np.random.uniform(-np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, hidden_dim))
 	    Why_b = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (word_dim, hidden_dim))
 	    Whh_b = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (hidden_dim, hidden_dim))
 	    # Theano: Created shared variables
 	    self.Wxh_b = theano.shared(name='Wxh_b', value=Wxh_b.astype(theano.config.floatX))
 	    self.Why_b = theano.shared(name='Why_b', value=Why_b.astype(theano.config.floatX))
 	    self.Whh_b = theano.shared(name='Whh_b', value=Whh_b.astype(theano.config.floatX))
-	    self.Whh_fb = theano.shared(name='Whh_fb', value=Whh_fb.astype(theano.config.floatX))
+	    # self.Whh_fb = theano.shared(name='Whh_fb', value=Whh_fb.astype(theano.config.floatX))
 
 	    bhh_f = np.zeros((hidden_dim,), dtype=theano.config.floatX)
 	    bhh_b = np.zeros((hidden_dim,), dtype=theano.config.floatX)
@@ -61,18 +61,18 @@ class BIRNN(object):
 			# print T.shape(U)
 			# print st_prev.ndim
 			# exit()
-			s_t = T.tanh(U[:,a_x] + W.dot(a_s_t) + Wbar.dot(a_hf_states) + bh)
+			s_t = T.tanh(U[:,a_x] + W.dot(a_s_t) + bh)
 			o_t = T.nnet.softmax(V.dot(s_t) + by)
 			return [o_t[0], s_t]
 
 		
 		
-		U, V, W, Wbar, bh, by = self.Wxh_b, self.Why_b, self.Whh_b, self.Whh_fb, self.bhh_b, self.bhy
+		U, V, W, bh, by = self.Wxh_b, self.Why_b, self.Whh_b, self.bhh_b, self.bhy
 		[o, s], updates = theano.scan(
 			forward_passb,
-			sequences=[x,hf_states],
+			sequences=x,
 			outputs_info=[None, dict(initial=T.zeros(self.hidden_dim))],
-			non_sequences=[U, V, W, Wbar, bh, by],
+			non_sequences=[U, V, W, bh, by],
 			go_backwards=True
 			
 			)
@@ -81,7 +81,7 @@ class BIRNN(object):
 		
 		o_error = T.sum(T.nnet.categorical_crossentropy(o[::-1], y))
 
-		params = [self.Wxh_f,self.Whh_f,self.Wxh_b,self.Why_b,self.Whh_b, self.Whh_fb, self.bhh_f,self.bhh_b,self.bhy]
+		params = [self.Wxh_f,self.Whh_f,self.Wxh_b,self.Why_b,self.Whh_b, self.bhh_f,self.bhh_b,self.bhy]
 		gparams = [T.grad(o_error, param) for param in params]
 		learning_rate = T.scalar('learning_rate')
 		clipped_gparams = [T.clip(gparam, T.ones_like(gparam)*-5.0, T.ones_like(gparam)*5.0) for gparam in gparams]
